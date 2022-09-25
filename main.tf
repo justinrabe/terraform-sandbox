77e7e753a872091f561c172313aa21c7310dd78c
Justin Rabe <jtr342@gmail.com>
2022-09-24 20:28:59 -0700

    created first AWS instance and VPC/VPC Subnet

---
+++ main.tf
@@ -0,0 +1,32 @@
+variable "a_key"{
+  type = string
+}
+variable "s_key"{
+  type = string
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.a_key
+  secret_key = var.s_key
+}
+# resource "aws_vpc" "vpc-test" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "production-vpc"
+#   }
+# }
+# resource "aws_subnet" "subnet1" {
+#   vpc_id     = aws_vpc.vpc-test.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "prod-subnet"
+#   }
+# }
+resource "aws_instance" "server-sandbox" {
+  ami           = "ami-026b57f3c383c2eec"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "test"
+  }
+}
