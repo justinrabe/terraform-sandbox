841bcf38b026f9116cf68b9dfafdb32b45457cdf
Justin Rabe <jtr342@gmail.com>
2022-09-28 19:40:49 -0700

    experimenting with VPCs

--- leader.tf
+++ leader.tf
@@ -9,20 +9,37 @@ provider "aws" {
   access_key = var.a_key
   secret_key = var.s_key
 }
+data "template_file" "user_data" {
+    template = file("ssms_install.ps1")
+}
 resource "aws_instance" "leader" {
   count = 1
   ami = "ami-0f1ee03d06c4c659c"
   instance_type = "t2.micro"
   key_name = "leader"
+  user_data = data.template_file.user_data.rendered
   tags = {
     Name = "leader-${count.index}"
   }
 }
+resource "aws_vpc" "vpc-test" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production-vpc"
+  }
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc-test.id
+  cidr_block = "10.0.1.0/24"
 
+  tags = {
+    Name = "prod-subnet"
+  }
+}
 resource "aws_security_group" "allow_rdp" {
   name        = "allow_rdp"
   description = "Allow RDP"
-  vpc_id      = "vpc-06806080a786563ca"
+  vpc_id      = aws_vpc.vpc-test.id
 
   ingress {
     description      = "RDP"
