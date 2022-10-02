d6e340e24c6d82d2b6d444c3b5f3cdc4ad09ef21
Justin Rabe <jtr342@gmail.com>
2022-10-02 10:52:59 -0700

    updated EIP dependency issues

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
-  depends_on = [aws_internet_gateway.gw]
+  depends_on = [aws_internet_gateway.gw , aws_instance.web-server]
 }
 
 resource "aws_instance" "web-server" {
