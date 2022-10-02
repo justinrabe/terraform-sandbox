77f1596d2ec8620f907eef23912b356da5f8b99e
Justin Rabe <jtr342@gmail.com>
2022-10-01 19:52:35 -0700

    created route table

--- main.tf
+++ main.tf
@@ -19,5 +19,22 @@ resource "aws_internet_gateway" "gw" {
     vpc_id = aws_vpc.prod-vpc.id
 }
 
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod-route-table"
+  }
+}
 
 
