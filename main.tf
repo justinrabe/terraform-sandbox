021685d989a79054d3299e3eb3cc1cd59984ab24
Justin Rabe <jtr342@gmail.com>
2022-10-01 19:56:17 -0700

    Added Subnet and route table association

--- main.tf
+++ main.tf
@@ -37,4 +37,19 @@ resource "aws_route_table" "prod-route-table" {
   }
 }
 
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
 
