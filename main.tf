b16a61227cccc3fac31409ddf57238cd5ca8e64d
Justin Rabe <jtr342@gmail.com>
2022-10-02 10:15:06 -0700

    setting up instance

--- main.tf
+++ main.tf
@@ -101,4 +101,16 @@ resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_instance" {
+  ami = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "leader"
+  network_interface {
+    network_interface_id = aws_network_interface.web-server-nic.id
+    device_index = 0
+  }
 }
